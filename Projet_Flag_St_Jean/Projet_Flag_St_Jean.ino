f0cc4330999cbcecd61d64d0c10588e653a953e5
giuseppe lomonaco <giuseppe.lomonaco@usherbrooke.ca>
2023-05-10 16:14:22 -0400

    final version 1.0

--- Projet_Flag_St_Jean/Projet_Flag_St_Jean.ino
+++ Projet_Flag_St_Jean/Projet_Flag_St_Jean.ino
@@ -1,6 +1,6 @@
 /*
    Created on     :   Mai 4, 2023
-   Last Update    :   Mai 4, 2023
+   Last Update    :   Mai 10, 2023
 
    Program Name   :
    Program File   :   Projet_Flag_St_Jean.ino
@@ -16,40 +16,19 @@
 */
 
 
-
-// adding test line for github
-
-
-
 #include <FastLED.h>
-//#include "functions.h"
-//#include "color_palettes.h"
-
-
 
 #define LED_TYPE        WS2812
 #define COLOR_ORDER     GRB
-
-
 #define DATA_PIN        6
-//#define BRIGHTNESS      50
 #define NUM_LEDS        200
-#define NMILLISECONDS   5
 
+unsigned long n_milliseconds = 5;
+uint8_t brightness = 50;
 
 // creating the led array
 CRGB leds[NUM_LEDS];
-
-
-
-unsigned long delayPeriod = 100;
-uint8_t brightness = 50;
-
-uint8_t hue = 0;  // to make sure  (0 <= hue <= 255)
-
-//uint8_t paletteIndex = 0;
-uint8_t colorIndex[NUM_LEDS];
-//uint8_t palIndex[NUM_LEDS];
+int colorIndex[NUM_LEDS];
 
 
 DEFINE_GRADIENT_PALETTE( heatmap_gp )  {
@@ -59,7 +38,6 @@ DEFINE_GRADIENT_PALETTE( heatmap_gp )  {
   255,  255,  255,  255         //full white  
 };
 
-
 DEFINE_GRADIENT_PALETTE( stjean_gp )  {
       0,      0,      255,    245,        //Cyan
     46,   0,      21,    255,        //dark blue
@@ -67,47 +45,13 @@ DEFINE_GRADIENT_PALETTE( stjean_gp )  {
       255,      0,      255,    245,        //Cyan
 };
 
-
-//// including palette from color_palettes.h
-//CRGBPalette16 greenBlue = greenblue_gp;
-CRGBPalette16 stJean = stjean_gp;
-CRGBPalette16 myPal = heatmap_gp;
+// creating palettes
+CRGBPalette16 stJean_palette = stjean_gp;
+CRGBPalette16 heat_palette = heatmap_gp;
 
 
 
-//
-//CRGBPalette16 purplePalette = CRGBPalette16 (
-//  CRGB::DarkViolet,
-//  CRGB::DarkViolet,
-//  CRGB::DarkViolet,
-//  CRGB::DarkViolet,
-//
-//  CRGB::Magenta,
-//  CRGB::Magenta,
-//  CRGB::Linen,
-//  CRGB::Linen,
-//
-//  CRGB::Magenta,
-//  CRGB::Magenta,
-//  CRGB::DarkViolet,
-//  CRGB::DarkViolet,
-//
-//  CRGB::DarkViolet,
-//  CRGB::DarkViolet,
-//  CRGB::Linen,
-//  CRGB::Linen
-//);
-
-
-
-
-
-
-
-
-
-
-// ****************************************************************************
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
   
     Serial.begin(115200);
@@ -125,44 +69,18 @@ void setup() {
     
     delay(1000);
 }
-// ****************************************************************************
 
 
-
-
-
-
-
-
-
-
-
-
-
-// ****************************************************************************
+// /////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
 
-  float red = CRGB::Red;
-  float white = CRGB::White;
-  float blue = CRGB::Blue;
-
-
-    EVERY_N_MILLISECONDS(NMILLISECONDS)  {
+    EVERY_N_MILLISECONDS(n_milliseconds)  {
       // switch on an LED at random, choosing a random color from the palette
-      leds[random8(0, NUM_LEDS - 1)] = ColorFromPalette(stJean, random8(), 255, LINEARBLEND);
+      leds[random8(0, NUM_LEDS - 1)] = ColorFromPalette(stJean_palette, random8(), 255, LINEARBLEND);
     }
 
     // Fade all LEDS down by 1 in brightness each time this is called
     fadeToBlackBy(leds, NUM_LEDS, 1);
 
-
-
-
-    //blueHueRandomFunction(leds, NUM_LEDS);
-
-
-
     FastLED.show(); 
-
 }
-// ****************************************************************************
